f7c748de996c08ae123e109980ec6488342580f8
ztronics <61567612+webzf@users.noreply.github.com>
2026-08-06 00:44:10 +0100

    Refactor ProductCategoryPage initialization and generator

--- _plugins/product_categories.rb
+++ _plugins/product_categories.rb
@@ -6,19 +6,19 @@ module Jekyll
 
       @site = site
       @base = base
-      @dir  = dir
+      @dir = dir
       @name = "index.html"
 
       process(@name)
 
-      read_yaml(
-        File.join(base, "_layouts"),
-        "product-category.html"
-      )
+      self.content = ""
 
-      self.data["title"] = category
-      self.data["category"] = category
-      self.data["permalink"] = "/products/#{Jekyll::Utils.slugify(category)}/"
+      self.data = {
+        "layout" => "product-category",
+        "title" => category,
+        "category" => category,
+        "permalink" => "/products/#{Jekyll::Utils.slugify(category)}/"
+      }
 
     end
 
@@ -27,28 +27,25 @@ module Jekyll
   class ProductCategoryGenerator < Generator
 
     safe true
-    priority :normal
+    priority :lowest
 
     def generate(site)
 
       return unless site.collections["products"]
 
-      categories =
-        site.collections["products"].docs
-            .map { |p| p.data["category"] }
-            .compact
-            .uniq
-            .sort
+      categories = site.collections["products"].docs
+        .map { |doc| doc.data["category"] }
+        .compact
+        .uniq
+        .sort
 
       categories.each do |category|
-
         site.pages << ProductCategoryPage.new(
           site,
           site.source,
           "products/#{Jekyll::Utils.slugify(category)}",
           category
         )
-
       end
 
     end
